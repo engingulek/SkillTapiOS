75e61f07615c0dad394540f3a00ef2fbb9f0c409
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2024-12-09 13:28:19 +0300

    created protocols for advertlistmodule

--- SkillTap/SkillTap/modules/AdvertListModule/AdvertListViewController.swift
+++ SkillTap/SkillTap/modules/AdvertListModule/AdvertListViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class AdvertListViewController: ViewController {
     private lazy var advertListView = AdvertListView(self)
+    lazy var presenter : ViewToPrensenterAdvertListProtocol = AdvertListPresenter(view: self)
     override func viewDidLoad() {
         super.viewDidLoad()
         view = advertListView
@@ -18,11 +19,10 @@ class AdvertListViewController: ViewController {
     }
 }
 
-
-
+//MARK: UICollectionViewDelegate,UICollectionViewDataSource
 extension AdvertListViewController : UICollectionViewDelegate,UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return presenter.numberOfItemsIn()
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -30,13 +30,34 @@ extension AdvertListViewController : UICollectionViewDelegate,UICollectionViewDa
             withReuseIdentifier: AdvertCVC.identifier,
             for: indexPath) as? AdvertCVC else {return UICollectionViewCell()}
         cell.configureData()
+        presenter.cellForItem(at: indexPath)
         return cell
     }
 }
 
 
+//MARK: UICollectionViewDelegateFlowLayout
 extension AdvertListViewController : UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: UIScreen.main.bounds.width - 10, height :UIScreen.main.bounds.height / 4)
+        return presenter.sizeForItemAt(width: UIScreen.main.bounds.width,
+                                       height: UIScreen.main.bounds.height)
     }
+}
+
+extension AdvertListViewController : PresenterToViewAdvertListProtocol {
+    func advertListCollectionViewPrepare() {
+        DispatchQueue.main.async {[weak self] in
+            guard let self = self else {return}
+            advertListView.advertCollectionViewViewPrepare()
+        }
+    }
+    
+    func advertListCollectionViewRealoaData() {
+        DispatchQueue.main.async {[weak self] in
+            guard let self = self else {return}
+            advertListView.advertCollectionViewVieRealoaData()
+        }
+    }
+    
+    
 }
